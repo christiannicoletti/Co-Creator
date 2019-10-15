9b75a2f41c60ab5f15f1566b99fec8610627eba3
christiannicoletti <cwnicoletti@gmail.com>
2019-10-14 18:44:41 -0700

    Re-creating default app with updated package

--- Application/src/App.js
+++ Application/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Site is under construction.
+          Site is under construction
         </p>
         <a
           className="App-link"
