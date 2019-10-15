ec0d4165a7844e729d7f5920c569fa531a4eeebb
christiannicoletti <cwnicoletti@gmail.com>
2019-10-14 17:12:41 -0700

    Temporarily editing landing page, before construction

--- Application/src/App.js
+++ Application/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site is under construction.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=u5KQQWlIgGc"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Funny video I found
         </a>
       </header>
     </div>
