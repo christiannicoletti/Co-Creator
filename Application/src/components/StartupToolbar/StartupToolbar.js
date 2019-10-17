e39c57fdac8751b3b9c6f776a4a00cd8748bd998
christiannicoletti <cwnicoletti@gmail.com>
2019-10-17 15:20:25 -0700

    Moving className call to header in toolbar

--- Application/src/components/StartupToolbar/StartupToolbar.js
+++ Application/src/components/StartupToolbar/StartupToolbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Searchbar from '../Searchbar/Searchbar';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './StartupToolbar.module.css';
 
 const toolbar = () => (
-    <header>
-        <nav className={classes.Main}>
+    <header className={classes.Main}>
+        <Searchbar />
+        <nav>
             <NavigationItems />
         </nav>
     </header>
