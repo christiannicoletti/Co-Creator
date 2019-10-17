fc267da8806f92651ca39885b74f035844d2f095
christiannicoletti <cwnicoletti@gmail.com>
2019-10-17 14:46:11 -0700

    Removing unnecessary div

--- Application/src/components/Layout/Layout.js
+++ Application/src/components/Layout/Layout.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import classes from './Layout.module.css';
-import WithClass from '../../hoc/withClass';
-import SideImage from '../SideImage/SideImage';
-import StartupToolbar from '../StartupToolbar/StartupToolbar';
-import Background from '../Background/Background';
+import classes from "./Layout.module.css";
+import WithClass from "../../hoc/withClass";
+import SideImage from "../SideImage/SideImage";
+import StartupToolbar from "../StartupToolbar/StartupToolbar";
+import Background from "../Background/Background";
 
 class Layout extends Component {
-    state = {}
-    render() { 
-        return (
-            <WithClass>
-                <div>
-                    <div className={classes.Background}>
-                        <Background />
-                    </div>
-                    <div className={classes.SideImage}>
-                        <SideImage />
-                    </div>
-                    <StartupToolbar />
-                    <div className={classes.Text}>
-                        <div>Start-up ui</div>
-                        <div>Start-up Footbar</div>
-                    </div>
-                </div>
-            </WithClass>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <WithClass>
+        <div className={classes.Background}>
+          <Background />
+        </div>
+        <div className={classes.SideImage}>
+          <SideImage />
+        </div>
+        <StartupToolbar />
+        <div className={classes.Text}>
+          <div>Start-up ui</div>
+          <div>Start-up Footbar</div>
+        </div>
+      </WithClass>
+    );
+  }
 }
- 
+
 export default Layout;
