3b3e7aa24242f474355170a22b4f213e17768ef0
christiannicoletti <cwnicoletti@gmail.com>
2019-10-17 21:44:26 -0700

    Correctly Pointing Route towards a component rather than the Layout

--- Application/src/App.js
+++ Application/src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 
 import "./App.css";
-import Layout from "./components/Layout/Layout";
+import StartPage from "./components/StartPage/StartPage";
+import WithClass from './hoc/withClass';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout>
+        <WithClass>
           <Switch>
-            <Route path="/" exact component={Layout} />
+            <Route path="/" exact component={StartPage} />
             <Redirect to="/" />
           </Switch>
-        </Layout>
+        </WithClass>
       </div>
     );
   }
