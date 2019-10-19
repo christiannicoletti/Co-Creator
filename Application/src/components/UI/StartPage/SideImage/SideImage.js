d02213b6dff69d114ad9894f7f1769399ac9e8c5
christiannicoletti <cwnicoletti@gmail.com>
2019-10-18 17:33:49 -0700

    Moving SideImage to UI directory and removing css file

---
+++ Application/src/components/UI/StartPage/SideImage/SideImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Image from "../../../../assets/images/StartupPage/StartupSidePicture.png";
+
+const sideimage = () => (
+  <div>
+    <img src={Image} />
+  </div>
+);
+
+export default sideimage;
