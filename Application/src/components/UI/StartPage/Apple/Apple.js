c3c12481ad48e0fabcf16033028c2e26a499b96d
christiannicoletti <cwnicoletti@gmail.com>
2019-10-18 17:34:42 -0700

    Moving Apple and GooglePlay icons to UI directory

---
+++ Application/src/components/UI/StartPage/Apple/Apple.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Image from "../../../../assets/images/apple.png";
+import classes from "./Apple.module.css";
+
+const logo = () => (
+  <div className={classes.Asset}>
+    <img src={Image} />
+  </div>
+);
+
+export default logo;
---
+++ Application/src/components/UI/StartPage/GooglePlay/GooglePlay.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Image from "../../../../assets/images/googleplay.png";
+import classes from "./GooglePlay.module.css";
+
+const logo = () => (
+  <div className={classes.Asset}>
+    <img src={Image} />
+  </div>
+);
+
+export default logo;
