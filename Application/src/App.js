f004e0ae5e9b757b255c2a91aa0a15a80c3ce780
christiannicoletti <cwnicoletti@gmail.com>
2019-10-18 17:24:14 -0700

    Removing <App> css files

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 
-import "./App.css";
 import StartPage from "./components/StartPage/StartPage";
-import WithClass from './hoc/withClass';
+import WithClass from "./hoc/withClass";
 
 class App extends Component {
   render() {
