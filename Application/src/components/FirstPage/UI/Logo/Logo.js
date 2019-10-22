469377842cf3dab4dd2a2386a0f0296ac9151a63
christiannicoletti <cwnicoletti@gmail.com>
2019-10-21 20:40:59 -0700

    <Logo> now takes local CSS and dynamic CSS

--- Application/src/components/FirstPage/UI/Logo/Logo.js
+++ Application/src/components/FirstPage/UI/Logo/Logo.js
@@ -3,8 +3,8 @@ import React from "react";
 import Image from "../../../../assets/images/FirstPage/Co-Creator.png";
 import classes from "./Logo.module.css";
 
-const logo = () => (
-    <img src={Image} className={classes.Asset}/>
+const logo = (props) => (
+    <img src={Image} className={`${classes.Asset} ${props.className}`}/>
 );
 
 export default logo;
