4b5c1af41d5d76ebe262ff7be340f54e75dbf5d1
christiannicoletti <cwnicoletti@gmail.com>
2019-10-22 14:10:37 -0700

    Reformatting CSS to look cleaner

--- Application/src/components/SigninPage/SigninPage.js
+++ Application/src/components/SigninPage/SigninPage.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 import classes from "./SigninPage.module.css";
 
 import WithClass from "../../hoc/withClass";
 import Background from "../shared/Background/Background";
-import Button from '../shared/UI/Buttons/Buttons';
-import Form from './Form/Form';
-import Layout from '../shared/Layout/Layout';
+import Button from "../shared/UI/Buttons/Buttons";
+import Form from "./Form/Form";
+import Layout from "../shared/Layout/Layout";
 
 /**
  * User Signin Page
@@ -21,13 +21,17 @@ class ui extends Component {
         <Background />
         <Layout />
         <div className={classes.Container}>
-          <Form className={classes.Form}/>
+          <Form className={classes.Form} />
           <div className={classes.ButtonContainer}>
-            <Button title="Sign in >" className={classes.Button}/>
+            <Button title="Sign in >" className={classes.Button} />
           </div>
           <div className={classes.TextContainer}>
-            <NavLink className={classes.ForgotPassword} to="forgotpassword">Forgot Password?</NavLink>
-            <NavLink className={classes.CreateAnAccount} to="signup">Create an account</NavLink>
+            <NavLink className={classes.ForgotPassword} to="forgotpassword">
+              Forgot Password?
+            </NavLink>
+            <NavLink className={classes.CreateAnAccount} to="signup">
+              Create an account
+            </NavLink>
           </div>
         </div>
       </WithClass>
