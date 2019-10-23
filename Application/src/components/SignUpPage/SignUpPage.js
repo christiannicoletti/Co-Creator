c61c0f751bcb98aa8a35afba0e1a2e2bda929979
christiannicoletti <cwnicoletti@gmail.com>
2019-10-22 23:02:39 -0700

    Removing components downwards from <Form> and putting into <Form> component

--- Application/src/components/SignUpPage/SignUpPage.js
+++ Application/src/components/SignUpPage/SignUpPage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
 
 import classes from "./SignupPage.module.css";
 
 import WithClass from "../../hoc/withClass";
 import Background from "../shared/Background/Background";
 import SideImage from './UI/SideImage/SideImage';
-import Button from '../shared/UI/Buttons/Buttons';
 import Form from './Form/Form';
 import Layout from '../shared/Layout/Layout';
 
@@ -26,14 +24,6 @@ class ui extends Component {
           <div className={classes.UiContainer}>
             <div className={classes.Text}>Sign Up</div>
             <Form className={classes.Form}/>
-            <div className={classes.Agreement}>
-              By clicking Create Account, you agree to our <NavLink to="Terms">Terms of Use</NavLink>, and <NavLink to="PrivacyPolicy">Privacy Policy</NavLink>.
-            </div>
-            <Button title="Create Account >" className={classes.Button}/>
-            <div className={classes.AlreadyHaveAccount}>
-              Already have an account?
-            </div>
-            <NavLink className={classes.SignIn} to="signin">Sign in</NavLink>
           </div>
         </div>
       </WithClass>
