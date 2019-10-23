6a49a7a963d4dd94fdeebc8765894823a7ba25be
christiannicoletti <cwnicoletti@gmail.com>
2019-10-22 23:07:05 -0700

    Adding disabled property to <Buttons> shared and modifying CSS

--- Application/src/components/shared/UI/Buttons/Buttons.js
+++ Application/src/components/shared/UI/Buttons/Buttons.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Buttons.module.css';
+import classes from "./Buttons.module.css";
 
 /**
  * Default cyan blue button
- * 
+ *
  * Dynamic title, pass any string to title to label the button
  */
-const button = (props) => (
-    <div className={`${classes.Default} ${props.className}`} onClick={props.clicked}>{props.title}</div>
+const button = props => (
+  <button
+    className={`${classes.Default} ${props.className}`}
+    onClick={props.clicked}
+    disabled={props.disabled}
+  >
+    {props.title}
+  </button>
 );
- 
+
 export default button;
