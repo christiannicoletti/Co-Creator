905bd5a391631a0fedda84d21a7855e1d465f467
christiannicoletti <cwnicoletti@gmail.com>
2019-10-24 02:09:56 -0700

    Added first header of ProjectsPage; Can switch between Projects/Startups

--- Application/src/components/ProjectsPage/ProjectsPage.js
+++ Application/src/components/ProjectsPage/ProjectsPage.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
+
+import classes from './ProjectsPage.module.css';
 
 import Layout from "../shared/Layout/Layout";
 import WithClass from "../../hoc/withClass";
@@ -10,11 +13,46 @@ import Background from "../shared/Background/Background";
  * Called by FirstPage.js in <Button>
  */
 class ui extends Component {
+  state = {
+    isCardView: true,
+    isListView: false
+  }
+
+  switchViewHandler = () => {
+    return this.setState({ isCardView: !this.state.isCardView, isListView: !this.state.isListView });
+  }
+
   render() {
+    let cardviewline = null;
+    if (this.state.isCardView) {
+      cardviewline = (
+        <div className={classes.CardViewLine}/>
+      )
+    }
+
+    let listviewline = null;
+    if (this.state.isListView) {
+      listviewline = (
+        <div className={classes.ListViewLine}/>
+      )
+    }
+
     return (
       <WithClass>
         <Background />
         <Layout />
+        <div className={classes.MainContainer}>
+          <div className={classes.ProjectsStartupsContainer}>
+            <div className={classes.Projects}>Projects</div>|<NavLink to="startups" className={classes.Startups}>Startups</NavLink>
+          </div>
+          {/* Insert Filter */}
+          <div className={classes.ViewsContainer}>
+            <div onClick={this.switchViewHandler} className={classes.Cardview}>Card view{cardviewline}</div><div onClick={this.switchViewHandler} className={classes.Listview}>List view{listviewline}</div>
+          </div>
+          {/* Insert Ranked by: */}
+          <div className={classes.CardViewContainer}>
+          </div>
+        </div>
       </WithClass>
     );
   }
