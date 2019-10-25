36f79b796593f10c8f3c7cc9261034af58d866bf
christiannicoletti <cwnicoletti@gmail.com>
2019-10-24 21:07:37 -0700

    Adding ContactPage Component

--- Application/src/App.js
+++ Application/src/App.js
@@ -5,9 +5,12 @@ import StartPage from "./components/FirstPage/FirstPage";
 import SignupPage from "./components/SignupPage/SignupPage";
 import SigninPage from "./components/SigninPage/SigninPage";
 import HowToStartPage from "./components/HowToStartPage/HowToStartPage";
+import ExplorePage from "./components/ExplorePage/ExplorePage";
 import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
 import StartupsPage from "./components/StartupsPage/StartupsPage";
 import AboutPage from "./components/AboutPage/AboutPage";
+import ContactPage from "./components/ContactPage/ContactPage";
+import HelpPage from "./components/HelpPage/HelpPage";
 import WithClass from "./hoc/withClass";
 
 /**
@@ -24,9 +27,12 @@ class App extends Component {
           <Route path="/signup" exact component={SignupPage} />
           <Route path="/signin" exact component={SigninPage} />
           <Route path="/howtostart" exact component={HowToStartPage} />
+          <Route path="/explore" exact component={ExplorePage} />
           <Route path="/projects" exact component={ProjectsPage} />
           <Route path="/startups" exact component={StartupsPage} />
           <Route path="/about" exact component={AboutPage} />
+          <Route path="/contact" exact component={ContactPage} />
+          <Route path="/help" exact component={HelpPage} />
           <Redirect to="/" /> {/* Redirect anything other than routes specified to "/" */}
         </Switch>
       </WithClass>
