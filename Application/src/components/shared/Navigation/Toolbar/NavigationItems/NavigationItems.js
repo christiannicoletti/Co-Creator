50ceac50b619213e1467ecdf3e150cf83c95af18
christiannicoletti <cwnicoletti@gmail.com>
2019-10-28 16:21:44 -0700

    Modifying Toolbar items to dynamically change on log-in

--- Application/src/components/shared/Navigation/Toolbar/NavigationItems/NavigationItems.js
+++ Application/src/components/shared/Navigation/Toolbar/NavigationItems/NavigationItems.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
+import ProfilePicture from '../../../../../assets/images/Toolbar/default_picture.png';
+import DownArrow from '../../../../../assets/images/Toolbar/down_arrow.png';
+import WithClass from '../../../../../hoc/withClass';
 
-const navigationItems = props => {
-  return (
-    <div className={`${classes.NavigationItems} ${props.className}`}>
+class navigationItems extends Component {
+  render() {
+    let navNotLoggedIn = (
+      <div className={`${classes.NavigationItems} ${this.props.className}`}>
       <NavigationItem link="/howtostart" exact>
         <strong>How to start</strong>
       </NavigationItem>
@@ -16,7 +20,32 @@ const navigationItems = props => {
         Sign in
       </NavigationItem>
     </div>
-  );
-};
+    )
 
+    let navLoggedIn = (
+      <div className={`${classes.NavigationItems} ${this.props.className}`}>
+      <NavigationItem link="/howtojoinproject" exact>
+        <strong>Help</strong>
+      </NavigationItem>
+      <NavigationItem link="/createproject" exact>
+        Create a project
+      </NavigationItem>
+      <NavigationItem link="/createproject" exact>
+        <div className={classes.ProfileContainer}>
+          <div className={classes.Username}>Christian Nicoletti</div>
+          <img src={ProfilePicture} className={classes.ProfilePicture} />
+          <img src={DownArrow} className={classes.DownArrow} />
+        </div>
+      </NavigationItem>
+    </div>
+    )
+
+    return (
+      <WithClass>
+        {navNotLoggedIn}
+      </WithClass>
+    );
+  }
+}
+ 
 export default navigationItems;
