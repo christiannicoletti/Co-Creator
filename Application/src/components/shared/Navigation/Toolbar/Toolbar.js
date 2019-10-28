35ab4c725d12c4e53515ca4985cdcea90ed21585
christiannicoletti <cwnicoletti@gmail.com>
2019-10-27 22:37:17 -0700

    Adding Home Icon to Toolbar

--- Application/src/components/shared/Navigation/Toolbar/Toolbar.js
+++ Application/src/components/shared/Navigation/Toolbar/Toolbar.js
@@ -4,11 +4,13 @@ import { NavLink } from "react-router-dom";
 import Searchbar from '../Searchbar/Searchbar';
 import NavigationItems from './NavigationItems/NavigationItems';
 import Logo from "../../Logo/Logo";
+import Home from '../../../../assets/images/home_icon.png';
 import classes from './Toolbar.module.css';
 
 const toolbar = () => (
     <header className={classes.Main}>
         <NavLink to="/"><Logo className={classes.Logo} /></NavLink>
+        <NavLink to="/" className={classes.Home}><img src={Home} className={classes.Home}/></NavLink>
         <Searchbar className={classes.Container1}/>
         <NavigationItems className={classes.Container2} />
     </header>
