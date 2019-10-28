08fec1f2449da4ea5bf5274b60bad4653cf968e0
christiannicoletti <cwnicoletti@gmail.com>
2019-10-28 16:20:32 -0700

    Modifying SubToolbar to soon dynamically change on log-in

--- Application/src/components/shared/Navigation/SubToolbar/NavigationItems/NavigationItems.js
+++ Application/src/components/shared/Navigation/SubToolbar/NavigationItems/NavigationItems.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
+import WithClass from '../../../../../hoc/withClass';
 
-const navigationItems = () => {
-  return (
-    <div className={classes.NavigationItems}>
-      <NavigationItem link="/explore" exact>
-        Explore
-      </NavigationItem>
-      <NavigationItem link="/projects" exact>
-        Projects
-      </NavigationItem>
-      <NavigationItem link="/startups" exact>
-        Startups
-      </NavigationItem>
-      <NavigationItem link="/about" exact>
-        About
-      </NavigationItem>
-      <NavigationItem link="/contact" exact>
-        Contact
-      </NavigationItem>
-    </div>
-  );
-};
+class navigationItems extends Component {
+  render() {
+    let navNotLoggedIn = (
+      <div className={classes.NavigationItems}>
+        <NavigationItem link="/explore" exact>
+          Explore
+        </NavigationItem>
+        <NavigationItem link="/projects" exact>
+          Projects
+        </NavigationItem>
+        <NavigationItem link="/startups" exact>
+          Startups
+        </NavigationItem>
+        <NavigationItem link="/about" exact>
+          About
+        </NavigationItem>
+  
+        {/* Waiting to add Contact page, for when users log in*/}
+        {/* <NavigationItem link="/contact" exact>
+          Contact
+        </NavigationItem> */}
+  
+        {/* Idea to make a news tab for Co-Create news & updates */}
+        {/* <NavigationItem link="/news" exact>
+          News
+        </NavigationItem> */}
+      </div>
+    )
 
+    return (
+      <WithClass>
+        {navNotLoggedIn}
+      </WithClass>
+    );
+  }
+}
+ 
 export default navigationItems;
