8d9576b57c0f2e6082f1540c4bec5d8e93a2783a
christiannicoletti <cwnicoletti@gmail.com>
2019-10-30 16:37:18 -0700

    Subtoolbar changes when logged in (authenticated

--- Application/src/components/shared/Navigation/SubToolbar/NavigationItems/NavigationItems.js
+++ Application/src/components/shared/Navigation/SubToolbar/NavigationItems/NavigationItems.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
-import WithClass from '../../../../../hoc/withClass';
+import WithClass from "../../../../../hoc/withClass";
 
 class navigationItems extends Component {
   render() {
-    let navNotLoggedIn = (
+    let nav = (
       <div className={classes.NavigationItems}>
         <NavigationItem link="/explore" exact>
           Explore
@@ -20,25 +20,36 @@ class navigationItems extends Component {
         <NavigationItem link="/about" exact>
           About
         </NavigationItem>
-  
-        {/* Waiting to add Contact page, for when users log in*/}
-        {/* <NavigationItem link="/contact" exact>
-          Contact
-        </NavigationItem> */}
-  
-        {/* Idea to make a news tab for Co-Create news & updates */}
-        {/* <NavigationItem link="/news" exact>
-          News
-        </NavigationItem> */}
       </div>
-    )
-
-    return (
-      <WithClass>
-        {navNotLoggedIn}
-      </WithClass>
     );
+
+    if (this.props.isAuthenticated) {
+      nav = (
+        <div className={classes.NavigationItems}>
+          <NavigationItem link="/explore" exact>
+            Explore
+          </NavigationItem>
+          <NavigationItem link="/projects" exact>
+            Projects
+          </NavigationItem>
+          <NavigationItem link="/startups" exact>
+            Startups
+          </NavigationItem>
+          <NavigationItem link="/about" exact>
+            About
+          </NavigationItem>
+          <NavigationItem link="/contact" exact>
+            Contact
+          </NavigationItem>
+          <NavigationItem link="/news" exact>
+            News
+          </NavigationItem>
+        </div>
+      );
+    }
+
+    return <WithClass>{nav}</WithClass>;
   }
 }
- 
+
 export default navigationItems;
