bafd5bdeae8d3f91bec53a8a3f35ee7bf04c8424
christiannicoletti <cwnicoletti@gmail.com>
2019-10-30 16:34:05 -0700

    App.js now checks if the user is already logged in and keeps them logged in

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import asynchComponent from './hoc/asynchComponent/asyncComponent';
 
 import StartPage from "./components/FirstPage/FirstPage";
 import SignupPage from "./components/SignupPage/SignupPage";
-import SigninPage from "./components/SigninPage/SigninPage";
 import HowToStartPage from "./components/HowToStartPage/HowToStartPage";
 import ExplorePage from "./components/ExplorePage/ExplorePage";
 import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
@@ -12,6 +13,12 @@ import AboutPage from "./components/AboutPage/AboutPage";
 import ContactPage from "./components/ContactPage/ContactPage";
 import HelpPage from "./components/HelpPage/HelpPage";
 import WithClass from "./hoc/withClass";
+import Logout from './containers/shared/Logout/Logout';
+import * as actions from "./store/actions/index";
+
+const asyncSigninPage = asynchComponent(() => {
+  return import('./components/SigninPage/SigninPage')
+});
 
 /**
  * Contains switch routing to components
@@ -19,13 +26,17 @@ import WithClass from "./hoc/withClass";
  * Called by index.js in ReactDOM.render()
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
       <WithClass>
         <Switch>
           <Route path="/" exact component={StartPage} />
           <Route path="/signup" exact component={SignupPage} />
-          <Route path="/signin" exact component={SigninPage} />
+          <Route path="/signin" exact component={asyncSigninPage} />
           <Route path="/howtostart" exact component={HowToStartPage} />
           <Route path="/explore" exact component={ExplorePage} />
           <Route path="/projects" exact component={ProjectsPage} />
@@ -33,6 +44,7 @@ class App extends Component {
           <Route path="/about" exact component={AboutPage} />
           <Route path="/contact" exact component={ContactPage} />
           <Route path="/help" exact component={HelpPage} />
+          <Route path="/logout" component={Logout} />
           <Redirect to="/" /> {/* Redirect anything other than routes specified to "/" */}
         </Switch>
       </WithClass>
@@ -40,4 +52,23 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => {
+      dispatch(actions.authCheckState());
+    }
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
