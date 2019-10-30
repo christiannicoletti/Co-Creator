da269b14334d54393fb893fb40bdf90fe0e5346f
christiannicoletti <cwnicoletti@gmail.com>
2019-10-29 20:32:29 -0700

    Modifying SiginPage now logs in and redirects

--- Application/src/containers/SigninPage/Form/Form.js
+++ Application/src/containers/SigninPage/Form/Form.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
+import React, { Component } from "react";
+import { Redirect, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions/index";
 
-import classes from './Form.module.css';
+import classes from "./Form.module.css";
 
-import Logo from '../../../components/shared/Logo/Logo';
-import Input from '../../../components/SigninPage/UI/Input/Input';
-import { checkValidity, updateObject} from '../../../shared/utility';
+import Logo from "../../../components/shared/Logo/Logo";
+import Input from "../../../components/SigninPage/UI/Input/Input";
+import { checkValidity, updateObject } from "../../../shared/utility";
 import Button from "../../../components/shared/UI/Buttons/Buttons";
 
 class userform extends Component {
@@ -42,7 +44,23 @@ class userform extends Component {
       }
     },
     formIsValid: false,
-    loading: false
+    loading: false,
+    isSignup: false
+  };
+
+  componentDidMount() {
+    if (this.props.authRedirectPath !== "/") {
+      this.props.onSetAuthRedirectPath();
+    }
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.userForm.email.value,
+      this.state.userForm.password.value,
+      this.state.isSignup
+    );
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -72,21 +90,21 @@ class userform extends Component {
     this.setState({ userForm: updatedUserForm, formIsValid: formIsValid });
   };
 
-  signinClickedValidationHandler = (form) => {
+  signinClickedValidationHandler = form => {
     if (!form) {
       this.setState({ clicked: true });
     }
-  }
+  };
 
-  checkResetHandler = (value) => {
+  checkResetHandler = value => {
     if (value === "") {
-      this.setState({ 
+      this.setState({
         valid: false,
         touched: false,
-        clicked: false 
+        clicked: false
       });
     }
-  }
+  };
 
   render() {
     const formElementsArray = [];
@@ -98,45 +116,77 @@ class userform extends Component {
     }
 
     let form = (
-      <form>
-        {formElementsArray.map(formElement => (
-          <Input
-            key={formElement.id}
-            elementType={formElement.config.elementType}
-            elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}
-            invalid={!formElement.config.valid[0]}
-            invalidMessage={formElement.config.valid[1]}
-            invalidForm={!this.state.formIsValid && this.state.clicked}
-            shouldValidate={formElement.config.validation}
-            touched={formElement.config.touched}
-            changed={event => this.inputChangedHandler(event, formElement.id)}
+      <div className={classes.Input}>
+        <form onSubmit={this.submitHandler}>
+          {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              invalid={!formElement.config.valid[0]}
+              invalidMessage={formElement.config.valid[1]}
+              invalidForm={!this.state.formIsValid && this.state.clicked}
+              shouldValidate={formElement.config.validation}
+              touched={formElement.config.touched}
+              changed={event => this.inputChangedHandler(event, formElement.id)}
+            />
+          ))}
+          <Button
+            title="Sign in >"
+            className={classes.Button}
+            clicked={() =>
+              this.signinClickedValidationHandler(this.state.formIsValid)
+            }
           />
-        ))}
-      </form>
+        </form>
+      </div>
     );
+
+    let authRedirect = null;
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to={this.props.authRedirectPath} />;
+    }
 
     return (
       <div className={this.props.className}>
         <Logo className={classes.Logo} />
-        <div className={classes.Input}>
-          {form}
+        {authRedirect}
+        {form}
+        <div className={classes.TextContainer}>
+          <NavLink className={classes.ForgotPassword} to="forgotpassword">
+            Forgot Password?
+          </NavLink>
+          <NavLink className={classes.CreateAnAccount} to="signup">
+            Create an account
+          </NavLink>
         </div>
-          <Button 
-          title="Sign in >" 
-          className={classes.Button}
-          clicked={() => this.signinClickedValidationHandler(this.state.formIsValid)} />
-          <div className={classes.TextContainer}>
-            <NavLink className={classes.ForgotPassword} to="forgotpassword">
-              Forgot Password?
-            </NavLink>
-            <NavLink className={classes.CreateAnAccount} to="signup">
-              Create an account
-            </NavLink>
-          </div>
       </div>
     );
   }
 }
 
-export default userform;
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
+    authRedirectPath: state.auth.authRedirectPath
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (fullname, email, username, password, isSignup) => {
+      dispatch(actions.auth(fullname, email, password, username, isSignup));
+    },
+    onSetAuthRedirectPath: () => {
+      dispatch(actions.setAuthRedirectPath("/"));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(userform);
