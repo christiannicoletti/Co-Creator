67ed5a8acb4b31c9b4dc1664e6b2dbd3341faf33
christiannicoletti <cwnicoletti@gmail.com>
2019-10-31 17:33:00 -0700

    Adding cors wrapper function

--- Application/functions/index.js
+++ Application/functions/index.js
@@ -12,34 +12,35 @@ app.use(authenticate); // firebase authentication middleware
 app.use(cors); // firebase cors
 
 
-exports.addUser = functions.https.onRequest(async (req, res) => {
-  /*
-  Trigger: whenever a new user signs up
-  Goal: Add the new user to the Users collection, also maybe send them a Welcome email?
-  */
-  const db = admin.firestore();
-  const {uid, email, username, photoURL, name} = req.body;
-  console.log("Here is req.body: ", req.body)
-  
+exports.addUser = functions.https.onRequest((req, res) => {
+  cors(async (req, res) => {
+    /*
+    Trigger: whenever a new user signs up
+    Goal: Add the new user to the Users collection, also maybe send them a Welcome email?
+    */
+    const db = admin.firestore();
+    const {uid, email, username, photoURL, name} = req.body;
+    console.log("Here is req.body: ", req.body)
+    
 
-  const data = {
-    email: email,
-    name: name,
-    username: username,
-    photo: photoURL,
-    dateCreated: admin.firestore.Timestamp.now()
-  }
-  console.log("Here is the user data ", user);
-  try {
-    let setUser = await db.collection('users').doc(uid).set(data);
-    console.log("Successfully created user!\n");
-    console.log(setUser);
-    console.log("User created at: ", setUser.writeTime);
-    res.status(201).send("User added to database!")
-  } catch (error) {
-    console.log("Error Storing user in database \n");
-    console.log(error);
-    res.status(400).send('Bad Request');
-  }
-
+    const data = {
+      email: email,
+      name: name,
+      username: username,
+      photo: photoURL,
+      dateCreated: admin.firestore.Timestamp.now()
+    }
+    console.log("Here is the user data ", user);
+    try {
+      let setUser = await db.collection('users').doc(uid).set(data);
+      console.log("Successfully created user!\n");
+      console.log(setUser);
+      console.log("User created at: ", setUser.writeTime);
+      res.status(201).send("User added to database!")
+    } catch (error) {
+      console.log("Error Storing user in database \n");
+      console.log(error);
+      res.status(400).send('Bad Request');
+    }
+  });
 })
