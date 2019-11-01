b5b06d62812d5dc19a200976c850d1808beca9fd
christiannicoletti <cwnicoletti@gmail.com>
2019-10-31 17:36:12 -0700

    Modifying bug, cors syntax

--- Application/functions/index.js
+++ Application/functions/index.js
@@ -12,8 +12,8 @@ app.use(authenticate); // firebase authentication middleware
 app.use(cors); // firebase cors
 
 
-exports.addUser = functions.https.onRequest((req, res) => {
-  cors(async (req, res) => {
+exports.addUser = functions.https.onRequest(async (req, res) => {
+  cors(request, response, () => {
     /*
     Trigger: whenever a new user signs up
     Goal: Add the new user to the Users collection, also maybe send them a Welcome email?
