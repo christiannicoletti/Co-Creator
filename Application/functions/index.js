5c2a3bd1b4c5007bbb2347a0bc1b1c743f35c1a7
christiannicoletti <cwnicoletti@gmail.com>
2019-11-03 02:11:18 -0800

    Modifying cloud functions to store in private and public collections

--- Application/functions/index.js
+++ Application/functions/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(authenticate); // firebase authentication middleware
 
-exports.addUser = functions.https.onRequest(async (req, res) => {
+exports.addPrivateUser = functions.https.onRequest(async (req, res) => {
   console.log("Setting CORS header");
   res.set("Access-Control-Allow-Origin", "*");
 
@@ -36,13 +36,20 @@ exports.addUser = functions.https.onRequest(async (req, res) => {
     };
     console.log("Here is userData: ", data);
     try {
-      let setUser = await db
-        .collection("users")
+      let setPrivateUser = await db
+        .collection("private_user_information")
         .doc(uid)
         .set(data);
       console.log("Successfully created user!\n");
-      console.log(setUser);
-      console.log("User created at: ", setUser.writeTime);
+      console.log(setPrivateUser);
+      console.log("User created at: ", setPrivateUser.writeTime);
+      let setPublicUser = await db
+        .collection("public_user_information")
+        .doc(username)
+        .set(data);
+      console.log("Successfully created user!\n");
+      console.log(setPublicUser);
+      console.log("User created at: ", setPublicUser.writeTime);
       res.status(201).send("User added to database!");
     } catch (error) {
       console.log("Error Storing user in database \n");
@@ -52,7 +59,7 @@ exports.addUser = functions.https.onRequest(async (req, res) => {
   }
 });
 
-exports.getUser = functions.https.onRequest(async (req, res) => {
+exports.getPrivateUser = functions.https.onRequest(async (req, res) => {
   console.log("Setting CORS header");
   res.set("Access-Control-Allow-Origin", "*");
 
@@ -69,7 +76,46 @@ exports.getUser = functions.https.onRequest(async (req, res) => {
     const { uid } = req.body;
     console.log("Here is req.body: ", req.body);
 
-    let docRef = db.collection("users").doc(uid);
+    let docRef = db.collection("private_user_information").doc(uid);
+
+    docRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          res.status(201).send(doc.data());
+          return doc.data();
+        } else {
+          // doc.data() will be undefined in this case
+          res.status(400).send("Bad Request");
+          return console.log("No such user!");
+        }
+      })
+      .catch(function(error) {
+        res.status(400).send("Bad Request");
+        return console.log("Error getting user:", error);
+      });
+  }
+});
+
+exports.getPublicUser = functions.https.onRequest(async (req, res) => {
+  console.log("Setting CORS header");
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    console.log("Setting CORS preflight options");
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+
+    res.status(201).send("CORS preflight options set!");
+  } else {
+    const db = admin.firestore();
+    const { username } = req.body;
+    console.log("Here is req.body: ", req.body);
+
+    let docRef = db.collection("public_user_information").doc(username);
 
     docRef
       .get()
