da198837c098a6f731c315c9175dc778d2692c51
christiannicoletti <cwnicoletti@gmail.com>
2019-11-04 20:02:41 -0800

    Modifying Spinner component to take in className

--- Application/src/components/shared/UI/Spinner/Spinner.js
+++ Application/src/components/shared/UI/Spinner/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import classes from './Spinner.module.css';
 
-const spinner = () => {
+const spinner = (props) => {
     return ( 
-        <div className={classes.loader}/>
+        <div className={`${classes.loader} ${props.className}`}/>
      );
 }
  
