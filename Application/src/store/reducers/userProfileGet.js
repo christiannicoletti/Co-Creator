793757eed49da45aecc3fbcc3f18f2f25fa97c13
christiannicoletti <cwnicoletti@gmail.com>
2019-11-10 19:39:31 -0800

    Modifying userProfileGet reducer vairables to name specific variables

--- Application/src/store/reducers/userProfileGet.js
+++ Application/src/store/reducers/userProfileGet.js
@@ -2,30 +2,31 @@ import * as actionTypes from "../actions/actionTypes";
 import { updateObject } from '../../shared/utility';
 
 const initialState = {
-  error: null,
-  loading: false,
-  complete: false
+  getUserProfileError: null,
+  getUserProfileLoading: false,
+  getUserProfileComplete: false
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_PUBLICUSERPROFILE_START:
       return updateObject(state, { 
-          error: null, 
-          loading: true 
+        getUserProfileComplete: false,
+        getUserProfileError: null, 
+        getUserProfileLoading: true 
         });
 
     case actionTypes.GET_PUBLICUSERPROFILE_SUCCESS:
       return updateObject(state, {
-        error: null,
-        loading: false,
-        complete: true
+        getUserProfileError: null,
+        getUserProfileLoading: false,
+        getUserProfileComplete: true
       });
 
     case actionTypes.GET_PUBLICUSERPROFILE_FAIL:
       return updateObject(state, {
-        error: action.error,
-        loading: false
+        getUserProfileError: action.error,
+        getUserProfileLoading: false
       });
 
     default:
