4e5a1fc70a29e086f4d30cb8ec37222ca3052057
christiannicoletti <cwnicoletti@gmail.com>
2019-11-10 19:35:18 -0800

    Biography form not works correctly & changing its directory

--- Application/src/containers/UserPage/biographyForm/Form.js
+++ Application/src/containers/UserPage/biographyForm/Form.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { Redirect, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../../../store/actions/index";
 
 import classes from "./Form.module.css";
 
-import Input from "../../../components/SignupPage/UI/Input/Input";
+import Input from "../../../components/UserPage/UI/Input/Input";
 import { checkValidity, updateObject } from "../../../shared/utility";
 import WithClass from "../../../hoc/withClass";
 import Button from "../../../components/shared/UI/Buttons/Buttons";
@@ -14,7 +13,7 @@ class userform extends Component {
   state = {
     workBiography: {
       information: {
-        elementType: "input",
+        elementType: "textarea",
         elementConfig: {
           type: "text",
           placeholder: "Enter your work biography..."
@@ -32,41 +31,44 @@ class userform extends Component {
   };
 
   submitHandler = event => {
+    let username = localStorage.getItem("publicuserinfousername");
     event.preventDefault();
-    this.props.onAuth(
-      this.state.userForm.information.value
+    this.props.submitBiographyForm(
+      username,
+      this.state.workBiography.information.value
     );
+    this.props.switchBiographyForm()
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
     // Updating form for each input letter from user
     const updatedFormElement = updateObject(
-      this.state.userForm[inputIdentifier],
+      this.state.workBiography[inputIdentifier],
       {
         value: event.target.value,
         valid: checkValidity(
           event.target.value,
-          this.state.userForm[inputIdentifier].validation
+          this.state.workBiography[inputIdentifier].validation
         ),
         touched: true
       }
     );
 
     // Updating userForm
-    const updatedUserForm = updateObject(this.state.userForm, {
+    const updatedWorkBiography = updateObject(this.state.workBiography, {
       [inputIdentifier]: updatedFormElement
     });
 
     // Checking if all of form is valid
     let formIsValid = true;
-    for (let inputIdentifier in updatedUserForm) {
-      formIsValid = updatedUserForm[inputIdentifier].valid[0] && formIsValid;
+    for (let inputIdentifier in updatedWorkBiography) {
+      formIsValid = updatedWorkBiography[inputIdentifier].valid[0] && formIsValid;
     }
 
     this.checkResetHandler(event.target.value);
 
     // Setting new state
-    this.setState({ userForm: updatedUserForm, formIsValid: formIsValid });
+    this.setState({ workBiography: updatedWorkBiography, formIsValid: formIsValid });
   };
 
   checkResetHandler = value => {
@@ -81,10 +83,10 @@ class userform extends Component {
   render() {
     // Making sure every element mapped has a key
     const formElementsArray = [];
-    for (let key in this.state.userForm) {
+    for (let key in this.state.workBiography) {
       formElementsArray.push({
         id: key,
-        config: this.state.userForm[key]
+        config: this.state.workBiography[key]
       });
     }
 
@@ -102,13 +104,21 @@ class userform extends Component {
               shouldValidate={formElement.config.validation}
               touched={formElement.config.touched}
               changed={event => this.inputChangedHandler(event, formElement.id)}
+              className={classes.Input}
             />
           ))}
-          <Button
-            title="Create Account "
-            className={classes.Button}
-            disabled={!this.state.formIsValid}
-          />
+          <div className={classes.ButtonsContainer}>
+            <Button
+              title="Submit "
+              className={classes.SubmitButton}
+              disabled={!this.state.formIsValid}
+            />
+            <Button
+              title="Cancel "
+              className={classes.CancelButton}
+              clicked={this.props.switchBiographyForm}
+            />
+          </div>
         </form>
       </WithClass>
     );
@@ -123,12 +133,19 @@ class userform extends Component {
 
 const mapStateToProps = state => {
   return {
-    loading: state.auth.loading,
-    complete: state.auth.complete,
-    error: state.auth.error,
+    postBiographyComplete: state.userProfilePost.postBiographyComplete
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    submitBiographyForm: (username, text) => {
+      dispatch(actions.postBiography(username, text));
+    }
   };
 };
 
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(userform);
